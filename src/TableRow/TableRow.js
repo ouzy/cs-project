b7784f8da248659b7dabf4790313c69b0a87de05
Ouzy <ousmane.mariko89@gmail.com>
2022-04-26 21:58:41 -0400

    quick tablerow structure

--- src/TableRow/TableRow.js
+++ src/TableRow/TableRow.js
@@ -1,5 +1,24 @@
+import { useState, useEffect } from "react";
 import "./TableRow.scss";
 
-export default function TableRow() {
-  return <div className="TableRow"></div>;
+export default function TableRow({ data }) {
+  const { name, device, path, status } = data;
+
+  const [isSelected, setIsSelected] = useState(false);
+
+  const handleRowToggle = () => {
+    setIsSelected(!isSelected);
+  };
+
+  return (
+    <div className="TableRow">
+      <button className="TableRowButton" onClick={handleRowToggle}>
+        <div className={`Checkbox ${isSelected ? "Checked" : ""}`}></div>
+        <span>{name}</span>
+        <span>{device}</span>
+        <span>{path}</span>
+        <span>{status}</span>
+      </button>
+    </div>
+  );
 }
